68f3421dee62b76e90c714ba9fa4efb0dd79fe7d
martinguerrero89 <martinguerrero89@users.noreply.github.com>
2017-08-27 20:15:14 -0300

    Update and rename variables.R to edit_this.R

--- edit_this.R
+++ edit_this.R
@@ -1,7 +1,7 @@
 #Proteomic challenge
 
 clonedir= "D:/Users/Marti/Downloads/elasticnet-master"
-datadir= paste(colnedir,"Data",sep="/")
+datadir= paste(clonedir,"Data",sep="/")
 
 source(paste(clonedir,"arrange.R",sep="/"))
 source(paste(clonedir,"evalscore.R",sep="/"))
@@ -10,8 +10,8 @@ source(paste(clonedir,"arrange.R",sep="/"))
 
 setwd(datadir)
 
-CNA= read.table("retrospective_breast_CNA_median_sort_common_gene_16884.txt",header=T,row.names=1,sep="\t")
-PROT= read.table("retrospective_breast_proteome_all_gene.txt",header=T,row.names=1,sep="\t")
+#CNA= read.table("retrospective_breast_CNA_median_sort_common_gene_16884.txt",header=T,row.names=1,sep="\t")
+#PROT= read.table("retrospective_breast_proteome_all_gene.txt",header=T,row.names=1,sep="\t")
 PROTF= read.table("retrospective_breast_proteome_filtered.txt",header=T,row.names=1,sep="\t")
 RNA= read.table("retrospective_breast_rna_seq_sort_common_gene_15115.txt",header=T,row.names=1,sep="\t")
 
